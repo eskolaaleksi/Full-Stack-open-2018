a5f6849e00f4448f28040858b2371919ac26fc20
eskolaaleksi <apesko@utu.fi>
2018-11-06 21:44:56 +0200

    Teht 1.4 alku

--- teht1.4/src/index.js
+++ teht1.4/src/index.js
@@ -1,12 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+    const kurssi = 'Half Stack -sovelluskehitys'
+    const osat = [
+        {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+        },
+        {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+        },
+        {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+        }
+    ]
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return (
+    <div>
+        <Otsikko kurssi={kurssi} />
+        <Sisalto osat={[osa1, osa2, osa3]}/>
+        <Yhteensa tehtavat={[osa1,osa2,osa3]}/>
+    </div>
+  )
+}
+
+const Otsikko = (props) => {
+    return (
+        <div>
+            <h1>{props.kurssi}</h1>
+        </div>
+    )
+}
+
+const Sisalto = (props) => {
+    return (
+        <div>
+            <Osa osa={props.osat[0].nimi} tehtava={props.osat[0].tehtavia}/>
+            <Osa osa={props.osat[1].nimi} tehtava={props.osat[1].tehtavia}/>
+            <Osa osa={props.osat[2].nimi} tehtava={props.osat[2].tehtavia}/>
+        </div>
+    )
+}
+
+const Osa = (props) => {
+    return (
+        <div>
+            <p>{props.osa} {props.tehtava}</p>
+        </div>
+    )
+}
+
+const Yhteensa = (props) => {
+    return (
+        <div>
+            <p>yhteensä {props.tehtavat[0].tehtavia + props.tehtavat[1].tehtavia + props.tehtavat[2].tehtavia} tehtävää</p>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
